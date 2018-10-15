fe5b78d1dfa9538f30051d0270bc784098af8837
Nicosoft <nicolas.jourdain@gmail.com>
2018-10-15 15:06:36 +1100

    Bump version

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'assignsubmission_ncmzoom';
-$plugin->release = '1.0.0';
-$plugin->version = 2018101500;
+$plugin->release = '1.0.1';
+$plugin->version = 2018101501;
 $plugin->requires = 2015111600;
 $plugin->maturity = MATURITY_STABLE;
